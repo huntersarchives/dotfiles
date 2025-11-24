50ad0d4e11d4914f52f432b5ef10bf2e2486a8f4
Hunter <hunterweisenbach@me.com>
2025-11-24 17:16:06 -0500

    Update nvim config to NvChad setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,67 +1,37 @@
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+vim.g.mapleader = " "
+
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
+
 vim.opt.rtp:prepend(lazypath)
 
--- Enable true color support
-vim.opt.termguicolors = true
+local lazy_config = require "configs.lazy"
 
--- Setup plugins
+-- load plugins
 require("lazy").setup({
-  -- Tree-sitter for syntax highlighting
   {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        -- Install parsers for common languages
-        ensure_installed = {
-          "lua",
-          "vim",
-          "vimdoc",
-          "python",
-          "javascript",
-          "typescript",
-          "html",
-          "css",
-          "json",
-          "yaml",
-          "markdown",
-          "bash",
-        },
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
-        -- Automatically install missing parsers when entering buffer
-        auto_install = true,
-        -- Enable syntax highlighting
-        highlight = {
-          enable = true,
-          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation)
-          additional_vim_regex_highlighting = false,
-        },
-        -- Enable indentation
-        indent = {
-          enable = true,
-        },
-      })
-    end,
-  },
-  -- Optional: A nice colorscheme
-  {
-    "folke/tokyonight.nvim",
+    "NvChad/NvChad",
     lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("tokyonight")
-    end,
+    branch = "v2.5",
+    import = "nvchad.plugins",
   },
-})
+
+  { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "options"
+require "autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
---
+++ nvim/lua/options.lua
@@ -0,0 +1,11 @@
+require "nvchad.options"
+
+-- add yours here!
+
+local o = vim.o
+o.expandtab = false  -- use tabs instead of spaces
+o.tabstop = 4        -- tab width
+o.shiftwidth = 4     -- indent width
+o.softtabstop = 4    -- backspace removes 4 spaces
+
+-- o.cursorlineopt ='both' -- to enable cursorline!
